1e47dcc045b4b95ecb34d41d1133d90d71cde6b1
Darakah <darakah.dark@gmail.com>
2021-03-06 16:59:58 +0100

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -4,5 +4,5 @@ export const VIEW_TYPE_OB_MAP_CONTROL = 'ob_map_control';
 export const VIEW_TYPE_OB_WORLD_MAP = 'ob_map';
 
 export const DEFAULT_SETTINGS: WorldMapSettings = {
-    mapTilesPath: 'app://local/Users/gaby/Desktop/Nehlam/Data/Tiles/{z}/{x}/{y}.png'
+    mapTilesPath: 'app://local/Users/Tiles/{z}/{x}/{y}.png'
 }
