901e9500172f611908638eb0910de657d7a3afd9
Darakah <darakah.dark@gmail.com>
2021-03-06 16:56:35 +0100

    Delete constants.ts

--- constants.ts
+++
@@ -1,8 +0,0 @@
-import { WorldMapSettings } from './types'
-
-export const VIEW_TYPE_OB_MAP_CONTROL = 'ob_map_control';
-export const VIEW_TYPE_OB_WORLD_MAP = 'ob_map';
-
-export const DEFAULT_SETTINGS: WorldMapSettings = {
-    mapTilesPath: 'app://local/Users/gaby/Desktop/Nehlam/Data/Tiles/{z}/{x}/{y}.png'
-}
