66ca1f78665d35a37f4f03f43f56b28d48c4ec74
Darakah <darakah.dark@gmail.com>
2021-03-15 21:33:45 +0100

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -3,6 +3,6 @@ import { WorldMapSettings } from './types'
 export const VIEW_TYPE_OB_WORLD_MAP = 'ob_map';
 
 export const DEFAULT_SETTINGS: WorldMapSettings = {
-    mapTilesPath: 'app://local/Users/gaby/Desktop/Nehlam/Data/Tiles/{z}/{x}/{y}.png',
+    mapTilesPath: 'app://local/Users/Data/Tiles/{z}/{x}/{y}.png',
     overlayData: []
 }
